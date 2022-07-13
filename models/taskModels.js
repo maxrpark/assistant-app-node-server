9d353d1e5e1d9703e34e0edd0d922bf597a161e4
Maxigong <maxi@nolgong.com>
2022-07-13 12:04:03 +0900

    fix model

--- models/taskModels.js
+++ models/taskModels.js
@@ -5,7 +5,7 @@ const TaskSchema = new mongoose.Schema({
     type: String,
     required: [true, "please enter task name"],
     trim: true,
-    maxlength: [20, "task name can not be more thatn 20 characters"],
+    // maxlength: [20, "task name can not be more thatn 20 characters"],
   },
   completed: {
     type: Boolean,
